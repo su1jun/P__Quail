7d0b9c2cf891ab4d8415d9c2aa595f367aef9429
su1jun <qaxsd530@gmail.com>
2024-05-03 23:24:28 +0900

    1

---
+++ Quail/src/main/java/org/zerock/quail/QuailApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.quail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuailApplication.class, args);
+    }
+
+}
